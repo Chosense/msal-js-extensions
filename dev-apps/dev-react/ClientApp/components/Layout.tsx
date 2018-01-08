354bc935be2f23732452eb3536ef2fa4d6437995
Mika Berglund <mika.berglund@chosense.com>
2018-01-08 02:03:14 +0200

    Added new dev app

---
+++ dev-apps/dev-react/ClientApp/components/Layout.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react';
+import { NavMenu } from './NavMenu';
+import { MsalService } from "../../../../src/MsalService";
+import { UserAgentApplication } from 'msal';
+
+export interface LayoutProps {
+    children?: React.ReactNode;
+}
+
+export class Layout extends React.Component<LayoutProps, {}> {
+    constructor() {
+        super();
+
+        var cb: any = undefined;
+
+        MsalService.initCurrent(
+            new UserAgentApplication("44d3dc9d-6185-40ff-9d5b-e32042d8b272", "https://login.microsoftonline.com/common", cb, {
+                redirectUri: "http://localhost:5000"
+            })
+        );
+    }
+
+
+    public render() {
+        return <div className='container-fluid'>
+            <div className='row'>
+                <div className='col-sm-3'>
+                    <NavMenu />
+                </div>
+                <div className='col-sm-9'>
+                    { this.props.children }
+                </div>
+            </div>
+        </div>;
+    }
+}
